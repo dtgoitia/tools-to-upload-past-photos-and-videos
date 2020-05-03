9a8e6f8edd30a9322ccc5b5663f42a9c58367654
David Torralba Goitia <david.torralba.goitia@gmail.com>
2020-05-03 15:51:11 +0100

    :sparkles: Compressed name for dirs and files

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import tarfile
 from typing import List
+import shutil
 
 
 def get_dirs(directory: Path) -> List[Path]:
@@ -29,25 +30,24 @@ def get_compressed_path(directory: Path) -> str:
         .replace(' - ', '_') \
         .replace(' ', '-')
 
-    compressed_path = Path.joinpath(Path.cwd(), f'{compressed_filename}.tar.lzma')
+    compressed_path = Path.joinpath(Path.cwd(), compressed_filename)
     return compressed_path
 
 
 def compress_dir(directory: Path) -> None:
-    print(get_compressed_path(directory))
+    path = get_compressed_path(directory)
+    shutil.make_archive(str(path), 'xztar')
+    shutil.rmtree(str(directory))
 
 
 def compress_files(files: List[Path]) -> None:
-    print(get_compressed_path(files[0].parent))
+    path = get_compressed_path(files[0].parent)
+    path = f'{path}.tar.xz'
 
-    # with tarfile.open(compressed_path, 'w') as tar:
-    #     for file in files:
-    #         tar.add(file)
-
-    for file in files:
-        pass
-        # print(f'  >> {file}')
-    # print(file)
+    with tarfile.open(path, 'w') as tar:
+        for file in files:
+            tar.add(file)  # compress
+            file.unlink()  # delete
 
 
 def main() -> None:
